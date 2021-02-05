624c5377b98b7328773adda56b2106dc43a3aa57
Sikó Márk - Levente <markforsapi@gmail.com>
2021-02-05 13:09:49 +0200

    Finalizing the Calculator App Adding Comments

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
+    // Represent whether the lastly pressed key is numeric or not
     var lastNumeric = false
+
+    // If true , do not allow to add another DOT
     var lastDot = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,101 +19,151 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onDigit(view:View){
+    /**
+     * Appends the numeric Button.text to the TextView
+     * @param view View
+     */
+    fun onDigit(view: View) {
         tvInput.append((view as Button).text)
         lastNumeric = true
     }
 
-    fun onClear(view: View){
-        tvInput.text=""
+    /**
+     * Clear the TextView
+     * @param view View
+     */
+    fun onClear(view: View) {
+        tvInput.text = ""
         lastNumeric = false
         lastDot = false
     }
 
-    fun onDecimalPoint(view: View){
-        if(lastNumeric && !lastDot){
+    /**
+     * Append DOT to the TextView
+     * @param view View
+     */
+    fun onDecimalPoint(view: View) {
+
+        // If the last appended value is numeric you can use DOT
+        if (lastNumeric && !lastDot) {
             tvInput.append(".")
             lastNumeric = false
             lastDot = true
         }
     }
 
-    fun onEqual(view: View){
-        if(lastNumeric){
+    /**
+     * Calculate the output
+     * @param view View
+     */
+    fun onEqual(view: View) {
+        if (lastNumeric) {
             var tvValue = tvInput.text.toString()
             var prefix = ""
-            try{
-                if(tvValue.startsWith("-")){
-                    prefix="-"
+            try {
+
+                // If the value starts with '-' then it will separate it
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
                     tvValue = tvValue.substring(1)
                 }
 
-                if(tvValue.contains("-")){
+                //If the inputValue contains the Division operator
+                if (tvValue.contains("-")) {
+                    //It will split the inputValue using Division operator
                     val splitValue = tvValue.split("-")
+
                     var one = splitValue[0]
                     var two = splitValue[1]
-                    if(!prefix.isEmpty()){
+
+                    //If the prefix is not empty than it will append it with first value
+                    if (!prefix.isEmpty()) {
                         one = prefix + one
                     }
+
+                    //The value one and two will be calculated based on the operator and it will display the result to the TV
                     tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
-                }else if(tvValue.contains("+")){
+                } else if (tvValue.contains("+")) {
                     val splitValue = tvValue.split("+")
+
                     var one = splitValue[0]
                     var two = splitValue[1]
-                    if(!prefix.isEmpty()){
+
+                    if (!prefix.isEmpty()) {
                         one = prefix + one
                     }
+
                     tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
-                }else if(tvValue.contains("/")){
+                } else if (tvValue.contains("/")) {
                     val splitValue = tvValue.split("/")
+
                     var one = splitValue[0]
                     var two = splitValue[1]
-                    if(!prefix.isEmpty()){
+
+                    if (!prefix.isEmpty()) {
                         one = prefix + one
                     }
+
                     tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
-                }else if(tvValue.contains("*")){
+                } else if (tvValue.contains("*")) {
                     val splitValue = tvValue.split("*")
+
                     var one = splitValue[0]
                     var two = splitValue[1]
-                    if(!prefix.isEmpty()){
+
+                    if (!prefix.isEmpty()) {
                         one = prefix + one
                     }
+
                     tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
                 }
 
-            }catch (e: ArithmeticException){
+            } catch (e: ArithmeticException) {
                 e.printStackTrace()
             }
         }
     }
 
-    private fun removeZeroAfterDot(result: String):String{
+    /**
+     * Remove the zero after decimal point
+     * @param result String
+     * @return String
+     */
+    private fun removeZeroAfterDot(result: String): String {
         var value = result
-        if(result.contains(".0")){
-            value=result.substring(0,result.length - 2)
+        if (result.contains(".0")) {
+            value = result.substring(0, result.length - 2)
         }
         return value
     }
 
-    fun onOperator(view: View){
-        if(lastNumeric && !isOperatorAdded(tvInput.text.toString())){
+    /**
+     * Append +,-,*,/ operators to the TextView
+     * @param view View
+     */
+    fun onOperator(view: View) {
+        if (lastNumeric && !isOperatorAdded(tvInput.text.toString())) {
             tvInput.append((view as Button).text)
             lastNumeric = false
             lastDot = false
         }
     }
 
-    private fun isOperatorAdded(value: String) : Boolean{
-        return if(value.startsWith("=")){
+    /**
+     * It's used to check whether any of the operator is used or not
+     * @param value String
+     * @return Boolean
+     */
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
             false
-        }else{
-            value.contains("/") || value.contains("*")
-                    || value.contains("+") || value.contains("-")
+        } else {
+            (value.contains("/")
+                    || value.contains("*")
+                    || value.contains("-")
+                    || value.contains("+"))
         }
     }
-
-
 
 
 }
